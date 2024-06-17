8562253771b77dfe48cbca737c128816ff7a200a
KyungjinLee <bramhome5848@gmail.com>
2024-06-17 21:24:18 +0900

    20240617 프로젝트 생성

---
+++ src/main/java/hello/springadvancedreview/SpringAdvancedReviewApplication.java
@@ -0,0 +1,13 @@
+package hello.springadvancedreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvancedReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/springadvancedreview/SpringAdvancedReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springadvancedreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvancedReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
